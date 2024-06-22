cff87162401eed516904f03ad9af4aeeda888a52
Acutapugione <acuta.pugione@gmail.com>
2024-06-22 15:08:10 +0300

    ...

--- 3_sapper/board.py
+++ 3_sapper/board.py
@@ -2,42 +2,73 @@ import random
 import time
 import pygame as pg
 from pygame.font import Font, SysFont
-from config import *
+from config import Config
+
+# (
+#     WIDTH,
+#     HEIGHT,
+#     SQ_AMOUNT,
+#     BOMBS_AMOUNT,
+#     FONT_FAMILY,
+#     SQ_SIZE,
+#     BOMB_COLOR,
+#     FLAG_COLOR,
+# )
 from square import Square
 
 
 class Board:
-
     def __init__(
         self,
-        squares_amount: int = SQ_AMOUNT,
-        font: Font = SysFont(FONT_FAMILY, size=70),
-        bombs_amount: int = BOMBS_AMOUNT,
+        squares_amount: int = Config.SQ_AMOUNT,
+        font: Font = SysFont(Config.FONT_FAMILY, size=70),
+        bombs_amount: int = Config.BOMBS_AMOUNT,
     ) -> None:
-        self.squares = tuple(
-            tuple(0 for row in range(squares_amount)) for col in range(squares_amount)
-        )
+        # self.__squares = tuple(
+        #     tuple(0 for row in range(squares_amount)) for col in range(squares_amount)
+        # )
         print(f"{self.squares=}")
         # self.squares = ((0) * squares_amount for x in range(squares_amount))
+        self.squares_amount = squares_amount
         self.bombs = set()
-        self.font = pg.font.SysFont(FONT_FAMILY, size=70)
+        self.font = font
         self.timer = 0
+        self.bombs_amount = bombs_amount
+
+        self.__squares = self.__generate_squares()
 
         self.generate_bombs()
-        self.create_board()
+        # self.create_board()
 
-    def create_board(self):
-        for row in range(SQ_AMOUNT):
-            for col in range(SQ_AMOUNT):
+    @property
+    def squares(self) -> list[list[Square]]:
+        result = []
+        for row in range(self.squares_amount):
+            result.append([])
+            for col in range(self.squares_amount):
                 if not (row, col) in self.bombs:
                     count = self.count_bombs_around(row, col)
-                    self.squares[row][col] = Square(row=row, col=col, count=count)
+                    result[-1].append(Square(row=row, col=col, count=count))
+        return result
+
+    def __generate_squares(self) -> tuple[tuple[int, ...], ...]:
+        return tuple(
+            tuple(0 for row in range(self.squares_amount))
+            for col in range(self.squares_amount)
+        )
+
+    # def create_board(self):
+    #     for row in range(SQ_AMOUNT):
+    #         for col in range(SQ_AMOUNT):
+    #             if not (row, col) in self.bombs:
+    #                 count = self.count_bombs_around(row, col)
+    #                 self.squares[row][col] = Square(row=row, col=col, count=count)
 
     def generate_bombs(self):
-        while len(self.bombs) != BOMBS_AMOUNT:
-            bomb_row, bomb_col = random.randint(0, SQ_AMOUNT - 1), random.randint(
-                0, SQ_AMOUNT - 1
-            )
+        while len(self.bombs) != self.bombs_amount:
+            bomb_row, bomb_col = random.randint(
+                0, Config.SQ_AMOUNT - 1
+            ), random.randint(0, Config.SQ_AMOUNT - 1)
             self.bombs.add((bomb_row, bomb_col))
             self.squares[bomb_row][bomb_col] = Square(
                 row=bomb_row, col=bomb_col, count=0, is_bomb=True
@@ -46,11 +77,12 @@ class Board:
     def count_bombs_around(self, row: int, col: int):
         count = 0
         for c_row in range(
-            row if row - 1 < 0 else row - 1, row + 1 if row + 2 > SQ_AMOUNT else row + 2
+            row if row - 1 < 0 else row - 1,
+            row + 1 if row + 2 > self.squares_amount else row + 2,
         ):
             for c_col in range(
                 col if col - 1 < 0 else col - 1,
-                col + 1 if col + 2 > SQ_AMOUNT else col + 2,
+                col + 1 if col + 2 > self.squares_amount else col + 2,
             ):
                 if (
                     self.squares[c_row][c_col].__class__ is Square
@@ -65,20 +97,21 @@ class Board:
             for square in row:
                 if square.with_flag:
                     rect = (
-                        square.col * SQ_SIZE,
-                        square.row * SQ_SIZE,
-                        SQ_SIZE,
-                        SQ_SIZE,
+                        square.col * Config.SQ_SIZE,
+                        square.row * Config.SQ_SIZE,
+                        Config.SQ_SIZE,
+                        Config.SQ_SIZE,
                     )
-                    pg.draw.rect(surface, FLAG_COLOR, rect)
+                    pg.draw.rect(surface, Config.FLAG_COLOR, rect)
 
     def open_few(self, row: int, col: int):
         for c_row in range(
-            row if row - 1 < 0 else row - 1, row + 1 if row + 2 > SQ_AMOUNT else row + 2
+            row if row - 1 < 0 else row - 1,
+            row + 1 if row + 2 > Config.SQ_AMOUNT else row + 2,
         ):
             for c_col in range(
                 col if col - 1 < 0 else col - 1,
-                col + 1 if col + 2 > SQ_AMOUNT else col + 2,
+                col + 1 if col + 2 > Config.SQ_AMOUNT else col + 2,
             ):
                 square = self.squares[c_row][c_col]
 
@@ -92,8 +125,13 @@ class Board:
 
     def draw_bombs(self, surface):
         for row, col in self.bombs:
-            rect = (col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE)
-            pg.draw.rect(surface, BOMB_COLOR, rect)
+            rect = (
+                col * Config.SQ_SIZE,
+                row * Config.SQ_SIZE,
+                Config.SQ_SIZE,
+                Config.SQ_SIZE,
+            )
+            pg.draw.rect(surface, Config.BOMB_COLOR, rect)
 
     def gameover_text(self, surface):
         ctime = time.time()
@@ -108,8 +146,8 @@ class Board:
         surface.blit(
             text_surface,
             (
-                (WIDTH // 2) - (text_surface.get_width() // 2),
-                (HEIGHT // 2) - (text_surface.get_height() // 2),
+                (Config.WIDTH // 2) - (text_surface.get_width() // 2),
+                (Config.HEIGHT // 2) - (text_surface.get_height() // 2),
             ),
         )
 
@@ -118,7 +156,7 @@ class Board:
         surface.blit(
             text_surface,
             (
-                (WIDTH // 2) - (text_surface.get_width() // 2),
-                (HEIGHT // 2) - (text_surface.get_height() // 2),
+                (Config.WIDTH // 2) - (text_surface.get_width() // 2),
+                (Config.HEIGHT // 2) - (text_surface.get_height() // 2),
             ),
         )
--- 3_sapper/config.py
+++ 3_sapper/config.py
@@ -1,19 +1,21 @@
 import sys
 
-SQ_AMOUNT = 20
 
-WIDTH, HEIGHT = (800 // SQ_AMOUNT) * SQ_AMOUNT, (800 // SQ_AMOUNT) * SQ_AMOUNT
-SQ_SIZE = WIDTH // SQ_AMOUNT
-BOMBS_AMOUNT = SQ_AMOUNT**2 // 10
+class Config:
+    SQ_AMOUNT = 20
 
-BG_COLOR = (107, 101, 100) # gray
-SQUARE_COLOR = (179, 176, 173) # light gray
-SQUARE_COLOR_VISIBLE = (125, 122, 120) # dark gray (border)
-BOMB_COLOR = (184, 38, 9) # red
-FLAG_COLOR = (242, 122, 27) # orange
+    WIDTH, HEIGHT = (800 // SQ_AMOUNT) * SQ_AMOUNT, (800 // SQ_AMOUNT) * SQ_AMOUNT
+    SQ_SIZE = WIDTH // SQ_AMOUNT
+    BOMBS_AMOUNT = SQ_AMOUNT**2 // 10
 
-FONT_SIZE = SQ_SIZE
-FONT_FAMILY ='Comic Sans MS'
+    BG_COLOR = (107, 101, 100)  # gray
+    SQUARE_COLOR = (179, 176, 173)  # light gray
+    SQUARE_COLOR_VISIBLE = (125, 122, 120)  # dark gray (border)
+    BOMB_COLOR = (184, 38, 9)  # red
+    FLAG_COLOR = (242, 122, 27)  # orange
+
+    FONT_SIZE = SQ_SIZE
+    FONT_FAMILY = "Comic Sans MS"
 
 
-sys.setrecursionlimit(999999999)
+# sys.setrecursionlimit(999999999)
